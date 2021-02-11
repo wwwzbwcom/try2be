4223423c08d333f8699e1a713a105f8d99172133
wwwzbwcom <zbwhome@outlook.com>
2021-02-11 21:36:31 +0800

    fix: remove changes from git

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function try2Ignore<T>(executeFunction: () => T): T | undefined;
-export declare function try2Arr<T>(executeFunction: () => T): [T | undefined, any];
-export declare function try2Obj<T>(executeFunction: () => T): {
-    val: T | undefined;
-    err: any;
-};
